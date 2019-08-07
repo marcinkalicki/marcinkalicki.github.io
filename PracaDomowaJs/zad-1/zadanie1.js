29bbd11497eb46cda608a488852f637b0e8f331a
Marcin Kalicki <marcink1w-wa@wp.pl>
2019-08-07 13:21:22 +0200

    zadanie 1 do sprawdzenia

---
+++ PracaDomowaJs/zad-1/zadanie1.js
@@ -0,0 +1,15 @@
+
+
+function reverseString(napis) {
+    let pobierzNapis;
+    let odwrocNapis;
+    let scalNapis;
+
+    pobierzNapis = napis.split('');
+    odwrocNapis = pobierzNapis.reverse();
+    scalNapis = odwrocNapis.join('');
+    
+    return scalNapis;
+}
+
+console.log( reverseString('Akademia 108') );
